81e7ed7c6d8af7e7a732a03ce57fe48543efab11
Maricar Walters <walterskai7@gmail.com>
2021-01-01 17:00:53 -0600

    Start refactoring code

--- TechJobsOO/JobField.cs
+++ TechJobsOO/JobField.cs
@@ -6,6 +6,8 @@ namespace TechJobsOO
 {
     public abstract class JobField
     {
-    
+        public int Id { get; }
+        private static int nextId = 1;
+        public string Value { get; set; }
     }
 }
